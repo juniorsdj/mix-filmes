83ef5b29facf91abcb9e5214a864322db39c9bfa
Junior <jddsjr98@gmail.com>
2019-08-05 08:45:04 -0300

    console dont working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,24 @@ import { StyleSheet, View, Text, TouchableOpacity, SafeAreaView } from 'react-na
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import navigatorService from './../helpers/NavigationService'
-import { colors } from './../helpers/Style'
-
+import { colors, normalizeWidPx, tipografy } from './../helpers/Style'
+import Requests from './../shared/Requests'
 const styles = StyleSheet.create({
     containerSafeAreaView: {
         flex: 1,
         backgroundColor: colors.black
     },
     container: {
+        flex: 1,
+        paddingVertical: normalizeWidPx(20),
+        paddingHorizontal: normalizeWidPx(12)
+    },
+    containerTela: {
         flex: 1
+    },
+    containerTitle: {
+        fontSize: tipografy.sizeTitle02,
+        color: colors.white
     },
     txt: {
         color: colors.white
@@ -24,17 +33,38 @@ const styles = StyleSheet.create({
 class Home extends Component {
     state = {
     };
+
+    componentDidMount(){
+        console.log('did mount')
+        Requests.discoverLancamentoThisMonth().then(r =>{
+            console.log(r)
+        }).catch(
+            err =>{
+                console.log(err)
+            }
+        )
+    }
     render() {
+        console.log('render')
         return (
             <SafeAreaView style={styles.containerSafeAreaView}>
                 <View style={styles.container}>
-                    <Text style={styles.txt}>Home</Text>
-                    <Text style={styles.txt}>Page</Text>
-                    <TouchableOpacity>
+                    <View style={styles.containerTela}>
+                        <Text style={styles.containerTitle}>Descubra</Text>
+                    </View>
+                    <View style={styles.containerTela}>
+                        <Text style={styles.containerTitle}>Filmes Populares</Text>
+                        <Text style={styles.txt}>Page</Text>
                         <Text style={styles.txt} onPress={() => navigatorService.navigate('Detalhes')}>
                             go to detalhes
                         </Text>
-                    </TouchableOpacity>
+                    </View>
+                    <View style={styles.containerTela}>
+                        <Text style={styles.containerTitle}>Atores/Atrizes</Text>
+                        <Text style={styles.txt}>fim</Text>
+                    </View>
+
+
                 </View>
             </SafeAreaView>
         )
